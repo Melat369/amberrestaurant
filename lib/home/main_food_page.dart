dec0064cdef494e8889a856c3d7ee8c92d76f52c
Melat369 <melattesfaye16@gmail.com>
2023-08-11 22:16:23 +0300

    putting the header row inside a column

--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -10,25 +10,37 @@ class MainFoodPage extends StatefulWidget {
 class _MainFoodPageState extends State<MainFoodPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      body:Column(
+        children:[
+           Container(
+     //the child basically represents the header section
       child: Container(
-        child:Row(
+       child:Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Column(
               children:[
-
+                Text('Country'),
+                Text('City')
               ]
-            ),
+            ) , 
             Container(
               width: 45,
               height:45,
-
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.pink, 
+              )
               
             )
-          ],
+          ], 
   
         )
       )
-    );
-  }
+    ),
+  
+        ]
+      )
+    );}
 }
