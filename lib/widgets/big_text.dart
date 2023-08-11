6581d774eee16a19d49dac1495963faadcbb8be3
Melat369 <melattesfaye16@gmail.com>
2023-08-11 22:53:17 +0300

    setting up properties inside the constructor

--- lib/widgets/big_text.dart
+++ lib/widgets/big_text.dart
@@ -3,14 +3,21 @@
 import 'package:flutter/material.dart';
 
 class BigText extends StatelessWidget {
-  final Color color;
-  //pass color variable
+  final Color? color;
+  //pass color variable it is optional
   final String text;
-  //pass in the text to be writtern
+  //pass in the text to be written
   double size;
   //for the dynamic text size
-
-  const BigText({super.key});
+  TextOverflow overflow;
+  //to tolerate long food names
+  const BigText({super.key,
+  this.color,
+  required this.text,
+  this.overflow = TextOverflow.ellipsis
+  //this is saying that if the text is overflowing show the too big
+  //default 3 dots to examplify that there is an overflow
+  });
 
   @override
   Widget build(BuildContext context) {
