07d8a000a022a4ce720579ed2c79b921901d7845
Melat369 <melattesfaye16@gmail.com>
2023-08-11 22:46:56 +0300

    custom text widget

---
+++ lib/widgets/big_text.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+
+class BigText extends StatelessWidget {
+  const BigText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text();
+  }
+}
