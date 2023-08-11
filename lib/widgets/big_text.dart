49ace170d240449ad1d9be039df17ccaa98d0a49
Melat369 <melattesfaye16@gmail.com>
2023-08-11 23:26:39 +0300

    setting the small text widget defaults

--- lib/widgets/big_text.dart
+++ lib/widgets/big_text.dart
@@ -9,16 +9,19 @@ class BigText extends StatelessWidget {
   //pass in the text to be written
   double size;
   //for the dynamic text size
+  double height;
+   
   TextOverflow overFlow;
   //to tolerate long food names
   BigText({super.key,
-  this.color = const Color(0xffcc7c5) ,
+  this.color = const Color(0xff89dad0) ,
   //setting default size
   this.size=20,
   required this.text,
+  this.height = 1.2,
   this.overFlow = TextOverflow.ellipsis
   //this is saying that if the text is overflowing show the too big
-  //default 3 dots to examplify that there is an overflow
+  //default 3 dots to exemplify that there is an overflow
   });
 
   @override
@@ -29,7 +32,8 @@ class BigText extends StatelessWidget {
       style: TextStyle(
         fontFamily: 'Roboto',
         color: color,
-        fontWeight: FontWeight.w400
+        fontWeight: FontWeight.w400,
+        fontSize: size,
       ),
 
     );
---
+++ lib/widgets/small_text.dart
@@ -0,0 +1,37 @@
+
+
+import 'package:flutter/material.dart';
+
+class SmallText extends StatelessWidget {
+  Color? color;
+  //pass color variable it is optional
+  final String text;
+  //pass in the text to be written
+  double size;
+  //for the dynamic text size
+  double height;
+  //the constructor
+  SmallText({super.key,
+  this.color = const Color(0xffcc7c5) ,
+  //setting default size
+  this.size=12,
+  this.height=1.2,
+  required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      //taking care of the over flow
+      style: TextStyle(
+        fontFamily: 'Roboto',
+        color: color,
+        fontSize: size,
+        
+        height: height
+      ),
+
+    );
+  }
+}
