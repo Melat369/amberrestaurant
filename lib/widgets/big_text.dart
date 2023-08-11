c693a76db2efcb25c3952561f093c69cbeb764a3
Melat369 <melattesfaye16@gmail.com>
2023-08-11 22:49:16 +0300

    creating custom variables

--- lib/widgets/big_text.dart
+++ lib/widgets/big_text.dart
@@ -3,10 +3,19 @@
 import 'package:flutter/material.dart';
 
 class BigText extends StatelessWidget {
+  final Color color;
+  //pass color variable
+  final String text;
+  //pass in the text to be writtern
+  double size;
+  //for the dynamic text size
+
   const BigText({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text();
+    return Text(
+
+    );
   }
 }
