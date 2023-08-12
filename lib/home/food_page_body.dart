e59f4235fc8ba86dc8c3b0596b1ec0e4428e8d4b
Melat369 <melattesfaye16@gmail.com>
2023-08-12 20:32:12 +0300

    created a stateful class for the body

---
+++ lib/home/food_page_body.dart
@@ -0,0 +1,15 @@
+//creating a stateful class
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
