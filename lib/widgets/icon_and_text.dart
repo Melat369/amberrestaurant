edabc0bd98b4a96d88e1805b3c91b71528556bc8
Melat369 <melattesfaye16@gmail.com>
2023-08-14 12:21:07 +0300

    created a reusable row widget

--- lib/widgets/icon_and_text.dart
+++ lib/widgets/icon_and_text.dart
@@ -1,11 +1,26 @@
+import 'package:amberrestaurant/widgets/small_text.dart';
 import 'package:flutter/material.dart';
 
 class IconAndTextWidget extends StatelessWidget {
-  
-  const IconAndTextWidget({super.key});
+  final IconData icon;
+  final String text;
+  final Color color;
+  final Color iconColor;
+  const IconAndTextWidget({super.key,
+   required this.icon,
+  required this.text,
+  required this.color,
+  required this.iconColor,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        Icon(icon, color:iconColor,),
+        SizedBox(width:5),
+        SmallText(text: text, color:color),
+      ],
+    );
   }
 }
