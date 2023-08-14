0d29e1ec37bfba227cf7d939e24e5d86e6966fb5
Melat369 <melattesfaye16@gmail.com>
2023-08-14 12:16:02 +0300

    declaring variables

---
+++ lib/widgets/icon_and_text.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class IconAndTextWidget extends StatelessWidget {
+  
+  const IconAndTextWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
